6b2db0fe49094d7edaf1185df1e5f8f3fdbf2242
ldiomande <lamdio2007@yahoo.fr>
2023-09-11 14:33:47 +0200

    Recup Exercice

--- src/WePlus/chap08/LectureEcritureFichier.java
+++ src/WePlus/chap08/LectureEcritureFichier.java
@@ -1,6 +1,5 @@
 package WePlus.chap08;
 
-import WePlus.chap08.Individu;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
@@ -8,34 +7,38 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.lang.Class.*;
 
 public class LectureEcritureFichier {
     public static void main(String args[]) {
 
+        ArrayList<Individu> lind = new ArrayList<>(); //Liste
+
+        /***Lecture fichier***/
         try {
             File fichier = new File("c:/Users/ldio/Formation_We+/Java/test_lecture.txt");
             FileReader lecteur = new FileReader(fichier);
             BufferedReader buffer = new BufferedReader(lecteur);
-            //Individu ind = new Individu(); //création d'un seul objet.
+            //Individu ind = new Individu(); //création d'un seul objet mis à jour à chaque passage dans la boucle.
+                                            //dernier enregistrement lu à la sortie de la boucle.
             String ligne;
-            
-            int i = 1;
-            ArrayList<Individu> lind = new ArrayList<>();
+            int numLigne = 1;
+            //ArrayList<Individu> lind = new ArrayList<>();
             //ListeIndividu lind = new ListeIndividu();
 
             System.out.println("------------------------Affichage lignes du fichier en entrée : \n");
 
             while ((ligne = buffer.readLine()) != null) {
-                if (i > 1) {
+                if (numLigne > 1) {
                     Individu ind = new Individu(); //création d'autant d'objets que de lignes.
-                    System.out.println("ligne : " + i);
+                    System.out.println("ligne numéro : " + numLigne);
                     System.out.println(ligne);
                     String col[] = ligne.split("#");
                     fillIndividu(ind, col);
                     System.out.println(ind.toString());
                     lind.add(ind);//stockage dans la liste des références d'objets crées à chaque itération.
                 }
-                i++;
+                numLigne++;
             }
 
             buffer.close();
@@ -46,21 +49,26 @@ public class LectureEcritureFichier {
             for (Individu element : lind) {
                 System.out.println("\n" + element.toString());
             }
-
-
-        }
-        catch(IOException e){
+        }catch(IOException e) {
                e.printStackTrace();
         }
-/*
+
+        /***Ecriture fichier : Listing***/
         try {
             File fichier = new File("c:/Users/ldio/Formation_We+/Java/test_ecriture.txt");
-            FileWriter ecrivain = new FileWriter(fichier);
+            FileWriter ecrivain = new FileWriter(fichier/*, true*/);
             BufferedWriter buffer = new BufferedWriter(ecrivain);
 
-            buffer.write("Hello, Java File Handling!");
+            /*buffer.write("ajout, Java File Handling!");
             buffer.newLine(); //Nouvelle ligne
-            buffer.write("Fin de lécriture.");
+            buffer.append("Fin de l'ajout.");*/
+
+            for (Individu element : lind) {
+                buffer.write(element.toString());
+                buffer.newLine(); //Nouvelle ligne
+                buffer.write("-----------------------------------------------");
+                buffer.newLine(); //Nouvelle ligne
+            }
 
             buffer.close();
             ecrivain.close();
@@ -68,21 +76,29 @@ public class LectureEcritureFichier {
             e.printStackTrace();
         }
 
+        /***Ecriture fichier : nouveau format***/
         try {
-            File fichier = new File("c:/Users/ldio/Formation_We+/Java/test_ecriture.txt");
-            FileWriter ecrivain = new FileWriter(fichier, true);
+            File fichier = new File("c:/Users/ldio/Formation_We+/Java/test_ecriture1.txt");
+            FileWriter ecrivain = new FileWriter(fichier/*, true*/);
             BufferedWriter buffer = new BufferedWriter(ecrivain);
 
-            buffer.write("ajout, Java File Handling!");
-            buffer.newLine(); //Nouvelle ligne
-            buffer.append("Fin de l'ajout.");
+            for (Individu element : lind) {
+                buffer.write(element.getNom() + "!" +
+                             element.getPrenom() + "!" +
+                             element.getDateNaissance() + "!" +
+                             element.getAdresse() + "!" +
+                             element.getComplement() + "!" +
+                             element.getCodePostal() + "!" +
+                             element.getVille()
+                            );
+                buffer.newLine(); //Nouvelle ligne
+            }
 
             buffer.close();
             ecrivain.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
- */
     }
 
     private static void fillIndividu(Individu ind, String[] col) {
