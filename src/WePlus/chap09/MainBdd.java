7dcdbb1accfb1c46461b6e306967c874a3c12af2
ldiomande <lamdio2007@yahoo.fr>
2023-09-14 14:41:35 +0200

    Recup Exercice

--- src/WePlus/chap09/MainBdd.java
+++ src/WePlus/chap09/MainBdd.java
@@ -1,11 +1,14 @@
 package WePlus.chap09;
 
 public class MainBdd {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         BddPostgreSql bdd = new BddPostgreSql("jdbc:postgresql://localhost:5432/weplus", "postgres", "Pangol2023!");
         bdd.testConnection();
-        //bdd.reqSelect("select * from public.\"Individu\"");
+        //bdd.reqSelect("select count(*) from public.\"Individu\"");
+        bdd.reqSelect("select * from public.\"Individu\"");
+        //bdd.reqSelect("select * from public.\"Etat_Civil\"");
         //bdd.reqExport("select * from public.\"Individu\"");
-        bdd.reqImport("insert into public.\"Sav_Individu\" values (");
+        //bdd.reqImport("insert into public.\"Sav_Individu\" values (");
+        //bdd.reqInsert("insert into public.\"Individu\" (nom, prenom, date_naissance, adresse, complement, code_postal, ville) values (");
     }
 }
