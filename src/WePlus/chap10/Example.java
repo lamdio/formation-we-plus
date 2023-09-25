873ad2f117e4a8acb346b74feb9b5c47819fde99
ldiomande <lamdio2007@yahoo.fr>
2023-09-25 10:23:35 +0200

    Recup Exercice

---
+++ src/WePlus/chap10/Example.java
@@ -0,0 +1,33 @@
+package WePlus.chap10;
+
+import java.util.*;
+interface StringConcat {
+    public String sconcat(String a, String b);
+}
+interface addFloat {
+    public float fadd(float x, float y);
+}
+public class Example {
+    public static void main(String args[]) {
+        // lambda expression with multiple arguments
+        StringConcat s = (str1, str2) -> str1 + str2;
+        System.out.println("Result: " + s.sconcat("Hello ", "World"));
+
+        addFloat af = (f1, f2) -> f1 + f2;
+        System.out.println("Result: " + af.fadd(42.3f, 12.7f));
+
+        List<String> list = new ArrayList<String>();
+        list.add("lolo");
+        list.add("manu");
+        list.add("gégé");
+        list.add("bribri");
+       /* for (int indice = 0; indice < list.size(); indice++) {
+            System.out.println(list.get(indice));
+        }*/
+        list.forEach (
+                //(names) -> System.out.println(names)
+                System.out::println
+        );
+
+    }
+}
